9a1d5b122b9a4fe1b1ac97e96706461ad4f5b1db
tranquangkhai2612 <tranquangkhai2612@gmail.com>
2023-09-28 19:44:54 +0700

    cv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { CVComponent } from './pages/cv/cv.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'signin',component:SigninComponent},
-  {path: 'signup', component:SigninComponent},
+  {path: 'signup', component:SignupComponent},
   {path: 'profile', component:ProfileComponent},
   {path: 'cv', component:CVComponent},
   {path: '404', component: Page404Component},
--- src/app/pages/cv/cv.component.ts
+++ src/app/pages/cv/cv.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css']
+  styleUrls: ['./cv.component.css','../../../assets/css/style_cv.css']
 })
 export class CVComponent implements OnInit {
 
